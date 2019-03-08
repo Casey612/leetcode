c0e22438b6dd964776b2269dbe7e69f1073c759d
yuki <cuiyuzhe@miaozhen.com>
2019-03-08 19:41:15 +0800

    [feature]30

---
+++ src/main/exec/RemoveElement.java
@@ -0,0 +1,23 @@
+package exec;
+
+/**
+ * @author yuzhe
+ * @since 3/8/19
+ */
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        for (int num : nums) {
+            if(num != val){
+                nums[result] = num;
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/main/exec/StrStr.java
@@ -0,0 +1,40 @@
+package exec;
+
+/**
+ * @author yuzhe
+ * @since 3/8/19
+ */
+public class StrStr {
+
+    public static int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        } else if (haystack.length() == needle.length()) {
+            if (haystack.equals(needle)) {
+                return 0;
+            } else {
+                return -1;
+            }
+        } else {
+
+            for (int start = 0; start <= (haystack.length() - needle.length()); start++) {
+                int i;
+                for (i = 0; i < needle.length(); i++) {
+                    if(haystack.charAt(start + i) != needle.charAt(i)){
+                        break;
+                    }
+                }
+                if(i == needle.length()){
+                    return start;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("hello", "ll"));
+        System.out.println(strStr("aaaaa", "bba"));
+    }
+
+}
