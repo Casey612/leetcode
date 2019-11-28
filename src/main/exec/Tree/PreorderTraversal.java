03021b898c4b0ca9375d7d1d12cd65f65cae2e2b
yuki <yuk.cyz@alibaba-inc.com>
2019-11-28 19:24:28 +0800

    [1 solution]binary tree pre-order traversal

---
+++ src/main/exec/Tree/PreorderTraversal.java
@@ -0,0 +1,14 @@
+package exec.Tree;
+
+import java.util.List;
+
+/**
+ * leetcode #144
+ */
+public class PreorderTraversal {
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return null;
+    }
+
+}
