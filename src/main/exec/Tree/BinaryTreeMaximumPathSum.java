e072b52716fdca8560129431014a709d98d6149e
yuki <yuk.cyz@alibaba-inc.com>
2020-01-16 15:18:39 +0800

    [1 solution]#124 solution

--- src/main/exec/Tree/BinaryTreeMaximumPathSum.java
+++ src/main/exec/Tree/BinaryTreeMaximumPathSum.java
@@ -8,6 +8,11 @@ public class BinaryTreeMaximumPathSum {
     int max = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
+       int result = travel(root);
+       return Math.max(result, max);
+    }
+
+    public int travel(TreeNode root) {
         if (root == null) {
             return 0;
         }
@@ -18,16 +23,37 @@ public class BinaryTreeMaximumPathSum {
             return cur;
         } else if (root.left != null && root.right != null) {
             //左右子节点都存在
-            int leftMax = maxPathSum(root.left);
-            int rightMax = maxPathSum(root.right);
+            int leftMax = travel(root.left);
+            int rightMax = travel(root.right);
 
+            //对上一层不可链接
+            max = Math.max(cur + leftMax + rightMax, max);
+            max = Math.max(rightMax, max);
+            max = Math.max(leftMax, max);
+
+            int temp = Math.max(leftMax, rightMax);
+            return Math.max(cur, temp + cur);
         } else {
             //只有一个子节点
             TreeNode node = root.left == null ? root.right : root.left;
-            int subMax = maxPathSum(node);
-
+            int subMax = travel(node);
+            max = Math.max(subMax, max);
+            return  Math.max(subMax + cur, cur);
         }
-        return 0;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(8);
+        TreeNode n1 = new TreeNode(9);
+        TreeNode n2 = new TreeNode(-6);
+        TreeNode n3 = new TreeNode(5);
+        TreeNode n4 = new TreeNode(9);
+        root.left = n1;
+        root.right = n2;
+        n2.left = n3;
+        n2.right = n4;
+        BinaryTreeMaximumPathSum summer = new BinaryTreeMaximumPathSum();
+        System.out.println(summer.maxPathSum(root));
     }
 
 }
